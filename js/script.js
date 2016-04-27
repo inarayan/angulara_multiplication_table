3d8aa5ffe8ae748789df080b6ce46eb61a3eede0
Indra Bijay <indrabijay007@gmail.com>
2016-04-26 21:16:18 -0700

    Implement the logic of determining upper limit for the table based on the users input

--- js/script.js
+++ js/script.js
@@ -2,11 +2,19 @@
  * Created by indrabijaynarayan on 4/24/16.
  */
 angular.module('myApp',[])
-.controller('MultiplicationCtrl',['$scope',function($scope){
-
-    $scope.numbers=function(){
-        var firstArray=[1,2,3,4,5,6,7,8,9,10];
-        return firstArray;
-    }
-
-}])
+.controller('MultiplicationCtrl', ['$scope', '$attrs', '$element', function($scope, $attrs, $element) {
+    $scope.upper_limit = 10;
+    $scope.$watch('upper_limit', function(newVal, oldVal) {
+        console.log(oldVal, newVal);
+        $scope.numbers = function () {
+        var firstArray = [];
+        for (var i = 1; i <= newVal; i++) {
+                firstArray.push(i);
+            }
+            return firstArray;
+        };
+        $scope.compute = function (a, b) {
+            return a * b;
+        }
+    });
+}]);
